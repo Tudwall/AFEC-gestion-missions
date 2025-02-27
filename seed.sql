310e21400d0773f92c6bba116f14a26e58105c4b
Pierre Laffaille <pierre.laffaille@outlook.fr>
2025-02-27 16:59:42 +0100

    updated scripts

--- seed.sql
+++ seed.sql
@@ -1,19 +1,19 @@
 USE gestionnaire;
 
 INSERT INTO
-    volunteer (name, surname, email, pwd)
+    volunteer (name, surname, email, pwd, createdOn)
 VALUES
-    ("John", "Doe", "john.doe@example.com", "john123"),
-    ("Jane", "Doe", "jane.doe@example.com", "jane123");
+    ("John", "Doe", "john.doe@example.com", "john123", "2025-02-27"),
+    ("Jane", "Doe", "jane.doe@example.com", "jane123", "2025-02-27");
 
 INSERT INTO
-    organization (name, email, pwd)
+    organization (name, email, pwd, createdOn)
 VALUES
-    ("BlueBand", "blueband@example.com", "blueband123"),
+    ("BlueBand", "blueband@example.com", "blueband123", "2025-02-27"),
     (
         "Croissantine",
         "contact@croissantine.com",
-        "croissant123"
+        "croissant123", "2025-02-27"
     );
 
 INSERT INTO
